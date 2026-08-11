13d33aef997992ec747fb76989c2008b5dd2b013
joecarper <baconking270@gmail.com>
2026-08-11 17:23:26 +0100

    Add strStr method to find substring index
    Implement strStr method to find the index of the first occurrence of a substring.

---
+++ substring.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        haystack = haystack.casefold()
+        haystack, needle = "sadbutsad", "sad"
+
+        if needle == "":
+            return 0
+
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i] == needle[0] and haystack[i:i + len(needle)] == needle:
+                return i
+
+        return -1
